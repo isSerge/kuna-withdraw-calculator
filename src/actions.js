7f7af3efe78ec48823b6b22e4c7a0bcdb61e99d6
Sergey Kovbasyuk <sergey.kovbasyuk@onereach.com>
2018-04-25 21:47:59 +0300

    update uah action

--- src/actions.js
+++ src/actions.js
@@ -1,32 +1,61 @@
 import axios from 'axios'
+import {
+	FETCH_KUNA_REQUEST,
+
+	FETCH_MARKET_REQUEST,
+	CHANGE_UAH_VALUE,
+} from './constants'
+
+const requestKuna = () => ({
+	type: FETCH_KUNA_REQUEST,
+})
+
+const requestMarket = () => ({
+	type: FETCH_MARKET_REQUEST,
+})
 
 export const fetchKuna = () => {
-	const url = 'https://kuna.io/api/v2/tickers'
-	const requests = this.state.currencies.map(x =>
-		axios.get(`${url}/${x.kunaName}uah/`).then(r => r.data.ticker.sell),
-	)
+	// const url = 'https://kuna.io/api/v2/tickers'
+	// const requests = this.state.currencies.map(x =>
+	// 	axios.get(`${url}/${x.kunaName}uah/`).then(r => r.data.ticker.sell),
+	// )
 
-	Promise.all(requests).then(values =>
-		this.setState(
-			this.state.currencies.map((x, i) => (x.rate = values[i])),
-		),
-	)
+	// Promise.all(requests).then(values =>
+	// 	this.setState(
+	// 		this.state.currencies.map((x, i) => (x.rate = values[i])),
+	// 	),
+	// )
+	console.log('fetchKuna')
+
+	return dispatch => {
+		dispatch(requestKuna())
+
+
+	}
 }
 
 export const fetchMarket = () => {
-	const url = 'https://api.coinmarketcap.com/v1/ticker'
-	const requests = this.state.currencies.map(x =>
-		axios.get(`${url}/${x.cmc}/`).then(r => ({
-			priceBtc: r.data[0].price_btc,
-			priceUsd: r.data[0].price_usd,
-		})),
-	)
+	// const url = 'https://api.coinmarketcap.com/v1/ticker'
+	// const requests = this.state.currencies.map(x =>
+	// 	axios.get(`${url}/${x.cmc}/`).then(r => ({
+	// 		priceBtc: r.data[0].price_btc,
+	// 		priceUsd: r.data[0].price_usd,
+	// 	})),
+	// )
 
-	Promise.all(requests).then(values =>
-		this.setState(
-			this.state.currencies.map((x, i) => (x.marketPrice = values[i])),
-		),
-	)
+	// Promise.all(requests).then(values =>
+	// 	this.setState(
+	// 		this.state.currencies.map((x, i) => (x.marketPrice = values[i])),
+	// 	),
+	// )
+	console.log('fetchMarket')
+
+	return dispatch => {
+		dispatch(requestMarket())
+	}
 }
 
-export const updateUah = value => {}
+export const updateUah = value => ({
+	type: CHANGE_UAH_VALUE,
+	value
+})
